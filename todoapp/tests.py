29071ae23dbddf62afbe4fa5c4ccdfaf484bc136
priyanka66 <priyanka@hashedin.com>
2014-07-09 19:34:15 +0530

    Test case to check public and private is implemented

--- todoapp/tests.py
+++ todoapp/tests.py
@@ -5,8 +5,16 @@ from django.contrib.auth.models import User
 class test_get_task(TestCase):
 	def setUp(self):
 		self.user = User.objects.create(username="priyanka",password="123",email = "asd@gmail.com")
-		task.objects.create(task_name="Task test",user_name = self.user)
+		task.objects.create(task_name="Task test",user_name = self.user,task_visibility="0")
 
 	def test_check_if_saved(self):
 		tasks = task.objects.filter(user_name = self.user)
 		self.assertEqual(len(tasks), 1, 'the task was not saved')
+
+	def test_get_public_tasks(self):
+		tasks=task.objects.filter(task_visibility="0")
+		self.assertEqual(len(tasks),1,'Succeeded')
+
+	def test_get_private_tasks(self):
+		tasks=task.objects.filter(task_visibility="1")
+		self.assertEqual(len(tasks),1,'FAiled')
